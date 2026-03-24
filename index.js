0efeb230e4240e3da1ee2425ae4d1182ad0f7f7c
shaskkkkkkkkkkkkkk <roma94808@gmail.com>
2026-03-24 12:30:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -10,65 +10,27 @@ if (!TOKEN) {
 
 // Rainbow roles (20 prestige roles)
 const rainbowRoleIds = [
-  '1447609483932205280', // Prestige 1: Frozen Age
-  '1447611391979819209', // Prestige 2: Wingful
-  '1447611757156630668', // Prestige 3: Wiking
-  '1449429902544605255', // Prestige 4: Valhalla
-  '1449429946014630072', // Prestige 5: Ragnarok
-  '1449429975215247400', // Prestige 6: Frostborn
-  '1449430012787687554', // Prestige 7: Cryo Lord
-  '1449430087249170582', // Prestige 8: Frostwing
-  '1449430129007657120', // Prestige 9: Ice Reign
-  '1449430190278053990', // Prestige 10: The snow has fallen
-  '1467244766826725478', // Prestige 11: Specialist
-  '1467245175645409325', // Prestige 12: Nightfall
-  '1467245620594081875', // Prestige 13: Celestial
-  '1467246031967354981', // Prestige 14: Influencer
-  '1467246602761797852', // Prestige 15: Initiate
-  '1476277302370369636', // Prestige 16: Eclipse
-  '1476277759893442611', // Prestige 17: Abyss
-  '1476278183341985993', // Prestige 18: Voidwalker
-  '1476278563283140701', // Prestige 19: Eternal Night
-  '1476279015689162934'  // Prestige 20: Godfall
+  '1447609483932205280',
+  '1447611391979819209',
+  '1447611757156630668',
+  '1449429902544605255',
+  '1449429946014630072',
+  '1449429975215247400',
+  '1449430012787687554',
+  '1449430087249170582',
+  '1449430129007657120',
+  '1449430190278053990',
+  '1467244766826725478',
+  '1467245175645409325',
+  '1467245620594081875',
+  '1467246031967354981',
+  '1467246602761797852',
+  '1476277302370369636',
+  '1476277759893442611',
+  '1476278183341985993',
+  '1476278563283140701',
+  '1476279015689162934'
 ];
-
-// Role IDs for ranks
-const rankRoles = {
-  1: '1485670782481727508',
-  2: '1485670811619561583',
-  3: '1485670833123885269',
-  4: '1485670859762040983',
-  5: '1485670891290366155',
-  6: '1485670913465651210',
-  7: '1485670933715882174',
-  8: '1485670953139703849',
-  9: '1485670975079977000',
-  10: '1485670993765732403'
-};
-
-// Role IDs for prestige
-const prestigeRoles = {
-  1: '1447609483932205280',
-  2: '1447611391979819209',
-  3: '1447611757156630668',
-  4: '1449429902544605255',
-  5: '1449429946014630072',
-  6: '1449429975215247400',
-  7: '1449430012787687554',
-  8: '1449430087249170582',
-  9: '1449430129007657120',
-  10: '1449430190278053990',
-  11: '1467244766826725478',
-  12: '1467245175645409325',
-  13: '1467245620594081875',
-  14: '1467246031967354981',
-  15: '1467246602761797852',
-  16: '1476277302370369636',
-  17: '1476277759893442611',
-  18: '1476278183341985993',
-  19: '1476278563283140701',
-  20: '1476279015689162934'
-};
 
 const client = new Client({
   intents: [
@@ -87,8 +49,6 @@ let whitelist = new Set();
 client.once('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
   console.log('Whitelist system ready!');
-  console.log(`Owner ID: ${OWNER_ID}`);
-  console.log('Rainbow cycle interval: 2 seconds');
 });
 
 // Черное оформление для успешной команды
@@ -107,8 +67,8 @@ async function sendSuccessNotification(channel, commandName, targetUser = null, 
     embed.addField('info', extraInfo, true);
   }
   
-  const message = await channel.send({ embeds: [embed] });
-  setTimeout(() => message.delete().catch(() => {}), 3000);
+  const msg = await channel.send({ embeds: [embed] });
+  setTimeout(() => msg.delete().catch(() => {}), 3000);
 }
 
 // Черное оформление для ошибки
@@ -119,8 +79,8 @@ async function sendErrorNotification(channel, errorMessage) {
     .setFooter({ text: '• command failed •' })
     .setTimestamp();
   
-  const message = await channel.send({ embeds: [embed] });
-  setTimeout(() => message.delete().catch(() => {}), 4000);
+  const msg = await channel.send({ embeds: [embed] });
+  setTimeout(() => msg.delete().catch(() => {}), 4000);
 }
 
 // Черное оформление для nuke
@@ -151,15 +111,15 @@ function getShipImage(percentage) {
 
 function getShipMessage(percentage, user1, user2) {
   if (percentage >= 90) {
-    return `💕 **${user1}** and **${user2}** are a PERFECT match! ${percentage}% Soulmates! 💕\nThey were meant to be together! 👨‍❤️‍👨`;
+    return `${percentage}% - Soulmates! They were meant to be together!`;
   } else if (percentage >= 70) {
-    return `💖 **${user1}** and **${user2}** are amazing together! ${percentage}% Great chemistry! 💖\nThis ship is sailing strong! ⛵`;
+    return `${percentage}% - Great chemistry! This ship is sailing strong!`;
   } else if (percentage >= 50) {
-    return `💛 **${user1}** and **${user2}** have potential! ${percentage}% Could work out! 💛\nGive it some time and see where it goes! 🌱`;
+    return `${percentage}% - Potential! Give it some time.`;
   } else if (percentage >= 30) {
-    return `💔 **${user1}** and **${user2}** might need more time... ${percentage}% Just friends? 💔\nSometimes friendship is better than love! 🤝`;
+    return `${percentage}% - Just friends? Sometimes friendship is better.`;
   } else {
-    return `💀 **${user1}** and **${user2}** are NOT compatible! ${percentage}% A disaster waiting to happen! 💀\nBetter to stay as acquaintances! 🚫`;
+    return `${percentage}% - Not compatible! Better to stay as acquaintances.`;
   }
 }
 
@@ -272,28 +232,26 @@ client.on('messageCreate', async (message) => {
       return;
     }
     
-    const user1 = message.mentions.members.first();
-    const user2 = message.mentions.members.array()[1];
+    const members = message.mentions.members.array();
+    const user1 = members[0];
+    const user2 = members[1];
     
     if (!user1 || !user2) {
       await sendErrorNotification(message.channel, 'please mention two users\nusage: :ship @user1 @user2');
       return;
     }
     
-    // Генерируем случайный процент совместимости
     const percentage = Math.floor(Math.random() * 101);
-    
-    // Создаем полоску прогресса
     const barLength = 20;
     const filledLength = Math.round((percentage / 100) * barLength);
     const bar = '█'.repeat(filledLength) + '░'.repeat(barLength - filledLength);
     
     const shipEmbed = new MessageEmbed()
       .setColor(0x000000)
-      .setTitle('💕 SHIP COMPATIBILITY 💕')
+      .setTitle('ship compatibility')
       .setDescription(`**${user1.user.username}** ❤️ **${user2.user.username}**`)
-      .addField('compatibility', `${percentage}% [${bar}]`, false)
-      .addField('result', getShipMessage(percentage, user1.user.username, user2.user.username), false)
+      .addField('compatibility', `\`\`\`\n${percentage}% [${bar}]\`\`\``, false)
+      .addField('result', `\`\`\`\n${getShipMessage(percentage, user1.user.username, user2.user.username)}\`\`\``, false)
       .setImage(getShipImage(percentage))
       .setFooter({ text: '• powered by love •' })
       .setTimestamp();
